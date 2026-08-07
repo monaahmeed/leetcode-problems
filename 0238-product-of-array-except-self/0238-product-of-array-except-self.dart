1e36ffdb72d09c7abc528c9a577ee90fac3bc580
mona <monaahmed2010250@gmail.com>
2026-08-07 15:21:58 +0300

    Time: 5 ms (93.71%), Space: 163.1 MB (95.1%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.dart
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.dart
@@ -1,23 +1,20 @@
 class Solution {
   List<int> productExceptSelf(List<int> nums) {
-   
     int n = nums.length;
-   List<int> p = List.filled(n, 1);
-   List<int> pi = List.filled(n, 1);
-   List<int> pii = List.filled(n, 1);
-    int left=1;
-    int right=1;
-    for(int i=0;i<n;i++){
-        p[i]=left;
-        left=left*nums[i];
+    List<int> res = List.filled(n, 1);
+    
+    int left = 1;
+    for (int i = 0; i < n; i++) {
+      res[i] = left;
+      left *= nums[i];
     }
-    for(int i=n-1;i>=0;i--){
-        pi[i]=right;
-        right=right*nums[i];
+
+   int right = 1;
+    for (int i = n - 1; i >= 0; i--) {
+      res[i] *= right;
+      right *= nums[i];
     }
-     for(int i=0;i<n;i++){
-       pii[i]=p[i]*pi[i];
-    }
-    return pii;
+
+    return res;
   }
 }
