d47479b527095297a0a59bbd9f88a88d8f913c89
mona <monaahmed2010250@gmail.com>
2026-08-10 18:17:17 +0300

    Time: 1 ms (100%), Space: 154.9 MB (80.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.dart
+++ 0283-move-zeroes/0283-move-zeroes.dart
@@ -1,18 +1,18 @@
 class Solution {
   void moveZeroes(List<int> nums) {
-    int j = 0;
-    for (int i = 0; i < nums.length; i++) {
+    int writeIndex = 0;
+    int n = nums.length;
+
+   for (int i = 0; i < n; i++) {
       if (nums[i] != 0) {
-       
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-        
-        j++;
+        nums[writeIndex] = nums[i];
+        writeIndex++;
       }
     }
-    for (int i = 0; i < nums.length; i++) {
-        print(nums[i]);
+
+   while (writeIndex < n) {
+      nums[writeIndex] = 0;
+      writeIndex++;
     }
   }
 }
