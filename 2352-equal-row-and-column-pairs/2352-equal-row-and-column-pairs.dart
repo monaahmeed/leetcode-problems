2bbb9ddded50f689fac3b6d89fca1d5ac9e842e4
mona <monaahmed2010250@gmail.com>
2026-08-25 23:10:37 +0300

    Time: 81 ms (100%), Space: 156.6 MB (100%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.dart
@@ -0,0 +1,27 @@
+class Solution {
+  int equalPairs(List<List<int>> grid) {
+    int n = grid.length;
+    int count = 0;
+
+    for (int r = 0; r < n; r++) {
+    
+      for (int c = 0; c < n; c++) {
+        
+        bool isEqual = true;
+       
+        for (int i = 0; i < n; i++) {
+          if (grid[r][i] != grid[i][c]) {
+            isEqual = false;
+            break;
+          }
+        }
+
+        if (isEqual) {
+          count++;
+        }
+      }
+    }
+
+    return count;
+  }
+}
